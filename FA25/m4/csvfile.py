92bb8dbeedfcec8eeb5a7c74f39c828e46815c34
cnelson20 <cjnelson2004@gmail.com>
2025-09-30 17:25:40 +0100

    today's meeting files

---
+++ FA25/m4/csvfile.py
@@ -0,0 +1,4 @@
+with open("cities.csv", "r") as file:
+	reader = csv.reader(file)
+	for row in reader:
+		print(row)
---
+++ FA25/m4/input.py
@@ -0,0 +1,5 @@
+text = input("Enter your prompt here: ");
+
+# print (text + len(text))
+
+print(text + str(len(text)))
